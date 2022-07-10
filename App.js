088dced3b74dcc04c20cf30e04ebb833f42b18a6
ceagar <ceagar123@yahoo.com>
2022-07-10 12:40:29 +0300

    First change in code for testing

--- App.js
+++ App.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 
 var port = process.env.PORT || 3000;
-app.get('/', (req, res) => res.send('Hello!'));
+app.get('/', (req, res) => res.send('Hello Larisa!'));
 
 app.listen(port, () => console.log('Server is running on port: ' + port));
